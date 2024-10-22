9d7ff01c57b10daecb07ed50815c19a7a5d6f89f
ginkoprim <93119906+ginkoprim@users.noreply.github.com>
2024-10-22 12:59:21 +0300

    Commit from TeleportHQ

---
+++ pages/test-page.js
@@ -0,0 +1,75 @@
+import React, { Fragment } from 'react'
+import Head from 'next/head'
+
+import { DataProvider, Repeater } from '@teleporthq/react-components'
+import { getEntityByAttribute } from '@teleporthq/cms-mappers/caisy'
+
+const TestPage = (props) => {
+  return (
+    <>
+      <div className="test-page-container">
+        <Head>
+          <title>test-page - Human Assurance Coordinator</title>
+          <meta
+            property="og:title"
+            content="test-page - Human Assurance Coordinator"
+          />
+        </Head>
+        <DataProvider
+          renderSuccess={(context_60ltsb) => (
+            <Fragment>
+              <h1>Heading</h1>
+              <h1>{context_60ltsb?.id}</h1>
+            </Fragment>
+          )}
+          params={{
+            projectId: '3bd8eb33-2aaa-4620-87bf-d7ccd04d0245',
+            query:
+              'query MyQuery{TypeWithRichText{_meta{createdAt updatedAt id}title content{json connections{__typename  }}}}',
+            attribute: 'id',
+            id: '2',
+          }}
+          initialData={props.context60ltsbProp}
+          persistDataDuringLoading={true}
+        />
+      </div>
+      <style jsx>
+        {`
+          .test-page-container {
+            width: 100%;
+            display: flex;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default TestPage
+
+export async function getStaticProps(context) {
+  try {
+    const context60ltsbProp = await getEntityByAttribute({
+      ...context?.params,
+      projectId: '3bd8eb33-2aaa-4620-87bf-d7ccd04d0245',
+      query:
+        'query MyQuery{TypeWithRichText{_meta{createdAt updatedAt id}title content{json connections{__typename  }}}}',
+      attribute: 'id',
+      id: '2',
+    })
+    return {
+      props: {
+        context60ltsbProp: context60ltsbProp?.data?.[0],
+      },
+      revalidate: 60,
+    }
+  } catch (error) {
+    return {
+      props: {},
+      revalidate: 60,
+    }
+  }
+}
